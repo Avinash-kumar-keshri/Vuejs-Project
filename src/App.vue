5dbaf9c4c615e2f34f265677debbbb15759d3485
Avinash-kumar-keshri <avinash.keshri@cssjockey.com>
2018-08-28 16:21:58 +0530

    Counter component created

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,20 @@
   <div id="app">
     <!--<img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>-->
-      <message>Hello World</message>
+      <counter></counter>
   </div>
 </template>
 
 <script>
 /*import HelloWorld from './components/HelloWorld.vue'*/
-import Message from './components/Message.vue'
+/*import Message from './components/Message.vue'*/
+import Counter from './components/Counter.vue'
+
 
 export default {
   name: 'app',
   components: {
-    Message
+    Counter
   }
 }
 </script>
